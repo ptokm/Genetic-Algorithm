fd7f961242292295baac0ca5838dd45ad6bea8d8
Paraskevi Tokmakidou <65904703+ptokm@users.noreply.github.com>
2022-10-03 20:04:25 +0300

    Changed in order to request input from user until he insert one of the given options

--- GeneticAlgorithm/src/optimizationAlgorithms/OptimizationAlgorithms.java
+++ GeneticAlgorithm/src/optimizationAlgorithms/OptimizationAlgorithms.java
@@ -25,36 +25,46 @@ public class OptimizationAlgorithms {
             System.out.println("How many genes do you want for each chromosome?");
             int genesCount = scanner.nextInt();
 
-            System.out.println("Enter the appropriate number from the list to select the initialization value of each chromosomes' genes:");
-            System.out.println("1 - Initialize all to value: 0");
-            System.out.println("2 - Initialize all to value: 1");
-            System.out.println("3 - Initialize all to random values 0 or 1");
-            String initializeGenesOption;
-            switch (scanner.nextInt()) {
-                case 1  -> initializeGenesOption = "0";
-                case 2  -> initializeGenesOption = "1";
-                case 3  -> initializeGenesOption = "random";
-                default -> initializeGenesOption = "random";
+            String initializeGenesOption = "";
+            while ( ! (initializeGenesOption.equalsIgnoreCase("0") || 
+                    initializeGenesOption.equalsIgnoreCase("1") ||
+                    initializeGenesOption.equalsIgnoreCase("random"))) {
+                System.out.println("Enter the appropriate number from the list to select the initialization value of each chromosomes genes:");
+                System.out.println("1 - Initialize all to value: 0");
+                System.out.println("2 - Initialize all to value: 1");
+                System.out.println("3 - Initialize all to random values 0 or 1");
+                
+                switch (scanner.nextInt()) {
+                    case 1  -> initializeGenesOption = "0";
+                    case 2  -> initializeGenesOption = "1";
+                    case 3  -> initializeGenesOption = "random";
+                }
             }
 
-            System.out.println("Enter the appropriate number from the list to select the fitness function to use:");
-            System.out.println("1 - sumation of genes");
-            System.out.println("2 - sumation of square genes");
-            String fitnessFunctionOption;
-            switch (scanner.nextInt()) {
-                case 1  -> fitnessFunctionOption = "sumation_of_genes";
-                case 2  -> fitnessFunctionOption = "sumation_of_square_genes";
-                default -> fitnessFunctionOption = "sumation_of_genes";
+            String fitnessFunctionOption = "";
+            while ( ! (fitnessFunctionOption.equalsIgnoreCase("sumation_of_genes") || 
+                      fitnessFunctionOption.equalsIgnoreCase("sumation_of_square_genes"))) {
+                System.out.println("Enter the appropriate number from the list to select the fitness function to use:");
+                System.out.println("1 - sumation of genes");
+                System.out.println("2 - sumation of square genes");
+                
+                switch (scanner.nextInt()) {
+                    case 1  -> fitnessFunctionOption = "sumation_of_genes";
+                    case 2  -> fitnessFunctionOption = "sumation_of_square_genes";
+                }
             }
-            
-            System.out.println("Enter the appropriate number from the list to select the crossover function to use:");
-            System.out.println("1 - single point");
-            System.out.println("2 - double point");
-            String crossoverFunctionOption;
-            switch (scanner.nextInt()) {
-                case 1  -> crossoverFunctionOption = "single_point";
-                case 2  -> crossoverFunctionOption = "double_point";
-                default -> crossoverFunctionOption = "single_point";
+
+            String crossoverFunctionOption = "";
+            while ( ! (crossoverFunctionOption.equalsIgnoreCase("single_point") || 
+                    crossoverFunctionOption.equalsIgnoreCase("double_point"))) {
+                System.out.println("Enter the appropriate number from the list to select the crossover function to use:");
+                System.out.println("1 - single point");
+                System.out.println("2 - double point");
+                
+                switch (scanner.nextInt()) {
+                    case 1  -> crossoverFunctionOption = "single_point";
+                    case 2  -> crossoverFunctionOption = "double_point";
+                }
             }
 
             Genetic_Algorithm genAlg = new Genetic_Algorithm(chromosomesCount, genesCount, initializeGenesOption, 
